c1e3334d51e9797a050e06303fcbf664b5fdedfc
Basit Ali <basitalimundia@gmail.com>
2016-06-18 16:09:04 +0500

    Make the Direction constants public.

--- CardStack/src/main/java/com/wenchao/cardstack/CardUtils.java
+++ CardStack/src/main/java/com/wenchao/cardstack/CardUtils.java
@@ -6,8 +6,8 @@ import android.widget.RelativeLayout;
 import android.widget.RelativeLayout.LayoutParams;
 
 public class CardUtils {
-    final static int DIRECTION_LEFT = 0;
-    final static int DIRECTION_RIGHT = 1;
+    public final static int DIRECTION_LEFT = 0;
+    public final static int DIRECTION_RIGHT = 1;
 
     public static void scale(View v, int pixel){
         RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams)v.getLayoutParams();
